71d179efec4968ae0c7b3abcf3f8b1cfe6b7e14c
Trigger <yehongjiang2012@gmail.com>
2016-04-09 12:49:32 +0800

    modify slice interface
    strcpy_s removed
    memcpy added

---
+++ RCDB/test_slice_list.cpp
@@ -0,0 +1,28 @@
+#include "slicelist.h"
+
+void main()
+{
+	SliceList* list = new SliceList;
+	unsigned char* key = new unsigned char[2];
+	unsigned char* value = new unsigned char[2];
+	key[0] = value[0] = 'a';
+	key[1] = value[1] = '\0';
+	list->add(Slice(key, 2, value, 2));
+	list->add(Slice(key, 2, value, 2));
+
+	SliceList::iterator ita;
+	ita = list->Begin();
+	while (!ita.isEmpty())
+	{
+		Slice slice = ita.next();
+	}
+	delete list;
+	//Slice a = Slice(key, 2, value, 2);
+	//if (true)
+	//{
+	//	Slice b;
+	//	b = a;
+	//}
+	//Slice c = a;
+
+}
